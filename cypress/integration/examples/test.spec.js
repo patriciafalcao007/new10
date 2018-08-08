5f69dc30a1c657783374ea857df30a38b9f019aa
patriciafalcao007@icloud.com <patriciafalcao007@icloud.com>
2018-08-08 17:41:59 +0200

    updates

--- cypress/integration/examples/test.spec.js
+++ cypress/integration/examples/test.spec.js
@@ -12,4 +12,23 @@ cy.get('input[name="login"]').type('patriciafalcao007@icloud.com')
 .get('input[type="submit"]').click()
 // .get('.user-nav').should('be.visible')
 });
+
+it ("Create a repo", () => { 
+    cy.visit("https://github.com/new")
+    .get('input[name="repository[name]"] ').type('repo-for-delete')
+    .get('input[name="repository[description]"] ').type('Delete the repo')
+    .get('input[type="submit"]').click()
+    .get("h1.public")
+    .should("have.text","repo-for-delete");
+    
+});
+
+it ("Delete the repo", () => { 
+    cy.visit("https://github.com/patriciafalcao007/repo-for-delete/settings")
+    .get('form').find('button').contains('Delete this repository').click()
+    .get('input[name="verify"] ').type('repo-for-delete')
+    .get('button.btn-danger').click()
+    .get(".container")
+    .should("have.text","Your repository \"repo-for-delete\" was successfully deleted.");
+}); 
 }); 
