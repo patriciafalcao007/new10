2a7e0ee343246bfd66c990fa4aadcd2de4de4c1d
jaffamonkey <paullittlebury@gmail.com>
2018-08-17 00:46:44 +0200

    updates

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
+
+
 Assignment JS
 
-git clone https://github.com/patriciafalcao007/new10.git
+```
+$ git clone https://github.com/patriciafalcao007/new10.git
+$ cd new10
 npm install
+````
 
 This will bring up the Cypress test management console:
+
 ```
 ./node_modules/.bin/cypress open 
 ```
@@ -11,5 +17,5 @@ This will bring up the Cypress test management console:
 This is example of how to run an indivudal test at the command line (also generates video recording of test:
 
 ```
-./node_modules/.bin/cypress run --spec 'cypress/integration/examples/login1.spec.js'
+$./node_modules/.bin/cypress run --spec 'cypress/integration/examples/login1.spec.js'
 ```
--- cypress/integration/examples/login2.spec.js
+++ cypress/integration/examples/login2.spec.js
@@ -1,15 +1,33 @@
-describe ("Login test 1 for New10", () => {
+describe ("Test Github login and create/delete a repo", () => {
     it ("Opens GitHub login page", () => { 
-        cy.visit("https://github.com/login")
-          .get("h1")
-          .should("have.text","Sign in to GitHub");
+        cy.visit("https://github.com/login");
+        cy.get("h1")
+        .should("have.text","Sign in to GitHub");
     });
     
     it ("Login to Github", () => { 
-        cy.get('input[name="login"]').type('patriciafalcao007@icloud.com')
-          .get('input[name="password"] ').type('falcaopatricia1')
-          .get('input[type="submit"]').click()
-          cy.wait(2000)
-          .get('.user-nav').should('be.visible');
+    cy.get('input[name="login"]').type('userid')
+    .get('input[name="password"] ').type('password')
+    .get('input[type="submit"]').click()
+    // .get('.user-nav').should('be.visible')
     });
+    
+    it ("Create a repo", () => { 
+        cy.visit("https://github.com/new")
+        .get('input[name="repository[name]"] ').type('repo-for-delete')
+        .get('input[name="repository[description]"] ').type('Delete the repo')
+        .get('input[type="submit"]').click()
+        .get("h1.public")
+        .should("have.text","repo-for-delete");
+        
+    });
+    
+    it ("Delete the repo", () => { 
+        cy.visit("https://github.com/patriciafalcao007/repo-for-delete/settings")
+        .get('form').find('button').contains('Delete this repository').click()
+        .get('input[name="verify"] ').type('repo-for-delete')
+        .get('button.btn-danger').click()
+        .get(".container")
+        .should("have.text","Your repository \"repo-for-delete\" was successfully deleted.");
     }); 
+}); 
